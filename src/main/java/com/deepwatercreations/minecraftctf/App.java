5845eeaf62af46735ba0d067070d4168caabea83
Dylan Craine <dylan.craine@deepwatercreations.com>
2021-05-02 19:24:45 -0600

    Generate maven project

---
+++ src/main/java/com/deepwatercreations/minecraftctf/App.java
@@ -0,0 +1,13 @@
+package com.deepwatercreations.minecraftctf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
