148847a258406011861974e22c9bef87a13b2baf
Dylan Craine <dylan.craine@deepwatercreations.com>
2021-05-08 20:10:31 -0600

    Set up initial files

---
+++ src/main/java/com/deepwatercreations/minecraftctf/MinecraftCTF.java
@@ -0,0 +1,19 @@
+package com.deepwatercreations.minecraftctf;
+
+
+public final class MinecraftCTF extends JavaPlugin{
+
+	@Override
+	public void onEnable(){
+
+	}
+
+	@Override
+	public void onDisable(){
+
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+	}
+}
